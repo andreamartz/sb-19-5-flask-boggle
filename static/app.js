f67fe772dd30313a25fa4d9ad683dbc23959e89b
andreamartz <14165788+andreamartz@users.noreply.github.com>
2020-08-08 16:17:29 -0500

    Add event handler for form submission

--- static/app.js
+++ static/app.js
@@ -3,3 +3,30 @@
 // **************************
 const $form = $("#word-check-form");
 let $messages = $("#messages");
+
+// **************************
+// EVENT HANDLERS
+// **************************
+
+$form.on("submit", async function (evt) {
+  evt.preventDefault();  // no page refresh
+  const url = "/word-check";
+  const $word = $("#word").val();
+  const res = await axios({
+    url: url,
+    method: "GET",
+    params: {
+      word: $word
+    }
+  });
+
+  let msg;
+  if (res.data.result === "ok") {
+    msg = "a valid word";
+  } else if (res.data.result === "not-on-board") {
+    msg = "not on the game board";
+  } else {
+    msg = "not a word";
+  }
+  $messages.text(`That's ${msg}.`);
+});
