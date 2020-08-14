790659a67725b60d098e220804a8a754d6a1d151
andreamartz <14165788+andreamartz@users.noreply.github.com>
2020-08-14 13:44:07 -0500

    Edit imports, config, & global variable

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask, session, request, redirect, render_template, jsonify
-from flask_debugtoolbar import DebugToolbarExtension
+# from flask_debugtoolbar import DebugToolbarExtension
 from boggle import Boggle
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "Gy6h72uIj0"
-app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+# app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+app.debug = True
 
-debug = DebugToolbarExtension(app)
+# debug = DebugToolbarExtension(app)
 boggle_game = Boggle()
 
 
